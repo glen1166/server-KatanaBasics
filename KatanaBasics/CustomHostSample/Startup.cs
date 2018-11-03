87f9e16e45d4ebbb285efc9b0c671d77d3836ccd
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-11-03 17:07:33 +0800

    self host

---
+++ KatanaBasics/CustomHostSample/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CustomHostSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, World!");
+            });
+        }
+    }
+}
